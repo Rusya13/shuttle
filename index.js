156bb0d8f15f62c581e4c6075897c3ed2333069a
Ruslan Osipov <rusya13@gmail.com>
2025-01-03 15:44:55 +1100

    test

--- index.js
+++ index.js
@@ -1,56 +1,59 @@
-import { createBareServer } from "@tomphttp/bare-server-node";
-import { createServer } from "node:http";
-import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-import { dynamicPath } from "@nebula-services/dynamic";
-import express from "express";
+import { createBareServer } from '@tomphttp/bare-server-node'
+import { createServer } from 'node:http'
+import { uvPath } from '@titaniumnetwork-dev/ultraviolet'
+import { dynamicPath } from '@nebula-services/dynamic'
+import express from 'express'
 
 const routes = [
-	["/", "index"],
-	["/math", "games"],
-	["/physics", "apps"],
-	["/settings", "settings"]
-];
+  ['/', 'index'],
+  ['/math', 'games'],
+  ['/physics', 'apps'],
+  ['/settings', 'settings'],
+]
 
 const navItems = [
-	["/", "Home"],
-	["/math", "Games"],
-	["/physics", "Apps"],
-	["/settings", "Settings"]
-];
+  ['/', 'Home'],
+  ['/math', 'Games'],
+  ['/physics', 'Apps'],
+  ['/settings', 'Settings'],
+]
 
-const bare = createBareServer("/bare/");
-const app = express();
+const bare = createBareServer('/bare/')
+const app = express()
 
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 
-app.use(express.static("./public"));
-app.use("/uv/", express.static(uvPath));
-app.use("/dynamic/", express.static(dynamicPath))
+app.use(express.static('./public'))
+app.use('/uv/', express.static(uvPath))
+app.use('/dynamic/', express.static(dynamicPath))
 
 for (const [path, page] of routes) {
-	app.get(path, (_, res) => res.render("layout", {
-		path,
-		navItems,
-		page
-	}));
+  app.get(path, (req, res) =>
+    res.render('layout', {
+      path,
+      navItems,
+      page,
+      domain: req.headers.host,
+    })
+  )
 }
 
-app.use((_, res) => res.status(404).render("404"));
+app.use((_, res) => res.status(404).render('404'))
 
-const httpServer = createServer();
+const httpServer = createServer()
 
-httpServer.on("request", (req, res) => {
-	if (bare.shouldRoute(req)) bare.routeRequest(req, res);
-	else app(req, res);
-});
+httpServer.on('request', (req, res) => {
+  if (bare.shouldRoute(req)) bare.routeRequest(req, res)
+  else app(req, res)
+})
 
-httpServer.on("error", (err) => console.log(err));
-httpServer.on("upgrade", (req, socket, head) => {
-	if (bare.shouldRoute(req)) bare.routeUpgrade(req, socket, head);
-	else socket.end();
-});
+httpServer.on('error', (err) => console.log(err))
+httpServer.on('upgrade', (req, socket, head) => {
+  if (bare.shouldRoute(req)) bare.routeUpgrade(req, socket, head)
+  else socket.end()
+})
 
 httpServer.listen({ port: process.env.PORT || 8080 }, () => {
-	const addr = httpServer.address();
-	console.log(`\x1b[42m\x1b[1m shuttle\n Port: ${addr.port}\x1b[0m`);
-});
+  const addr = httpServer.address()
+  console.log(`\x1b[42m\x1b[1m shuttle\n Port: ${addr.port}\x1b[0m`)
+})
